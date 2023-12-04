0c3bf8b359c003e89a4c1576eb6a0e0c2ecf5ee0
Roman Motorin <35640501+romanchyk84@users.noreply.github.com>
2023-12-04 14:49:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,4 +15,4 @@ echo "FINISH"
 
  #curl -X POST https://simple-books-api.glitch.me/api-clients -H 'Content-Type: application/json' -d '{"clientName": "Roman","clientEmail": "Roman@postman.com"}'
  #curl -H 'Authorization: Bearer 5dc3e9a5c28f4310753b7cd5f0be2ef8cae50c2a45f612185a1010c08f514747' -X PATCH https://simple-books-api.glitch.me/orders/GksFsZkhJIEn3fAaKWgmE -H 'Content-Type: application/json' -d '{"customerName": "Roman Roman"}'
- #$ curl -H 'Authorization: Bearer 5dc3e9a5c28f4310753b7cd5f0be2ef8cae50c2a45f612185a1010c08f514747' -X DELETE https://simple-books-api.glitch.me/orders/kfsP0UIrgL1AFkbGtMG_N
+ #curl -H 'Authorization: Bearer 5dc3e9a5c28f4310753b7cd5f0be2ef8cae50c2a45f612185a1010c08f514747' -X DELETE https://simple-books-api.glitch.me/orders/kfsP0UIrgL1AFkbGtMG_N
